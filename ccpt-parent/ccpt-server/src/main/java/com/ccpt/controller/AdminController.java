0cd6b109844d4241432055b0ffb6c639fdbffa2f
anuraguskcorp <anurag.uskcorp@gmail.com>
2019-04-26 12:14:45 +0530

    Admin controller

--- ccpt-parent/ccpt-server/src/main/java/com/ccpt/controller/AdminController.java
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/controller/AdminController.java
@@ -7,43 +7,41 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.ccpt.model.ClientApplicationStatus;
 import com.ccpt.model.ClientPositionStatus;
 import com.ccpt.model.ConsultantStatus;
-import com.ccpt.repository.ClientApplicationStatusRepository;
-import com.ccpt.repository.ClientPositionStatusRepository;
-import com.ccpt.repository.ConsultantStatusRepository;
+import com.ccpt.service.IClientApplicationStatusService;
+import com.ccpt.service.IClientPositionStatusService;
+import com.ccpt.service.IConsultantStatusService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	@Autowired
-	ClientApplicationStatusRepository clientApplicationStatusRepository;
-	@Autowired
-	ClientPositionStatusRepository clientPositionStatusRepository;
-	@Autowired
-	ConsultantStatusRepository consultantStatusRepository;
 
-	@PostMapping("/addClientApplicationStatus")
-	public ResponseEntity<Void> addClientApplicationStatus(@RequestBody ClientApplicationStatus clientApplicationStatus,
-			UriComponentsBuilder builder) {
-		clientApplicationStatusRepository.save(clientApplicationStatus);
+	@Autowired
+	private IClientApplicationStatusService clientApplicationStatusService;
+	@Autowired
+	private IClientPositionStatusService clientPositionStatusService;
+	@Autowired
+	private IConsultantStatusService consultantStatusService;
+
+	@PostMapping("addClientApplicationStatus")
+	public ResponseEntity<Void> addClientApplicationStatus(
+			@RequestBody ClientApplicationStatus clientApplicationStatus) {
+		clientApplicationStatusService.addClientApplicationStatus(clientApplicationStatus);
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
 
-	@PostMapping("/addClientPositionStatus")
-	public ResponseEntity<Void> addClientPositionStatus(@RequestBody ClientPositionStatus clientPositionStatus,
-			UriComponentsBuilder builder) {
-		clientPositionStatusRepository.save(clientPositionStatus);
+	@PostMapping("addClientPositionStatus")
+	public ResponseEntity<Void> addClientPositionStatus(@RequestBody ClientPositionStatus clientPositionStatus) {
+		clientPositionStatusService.addClientPositionStatus(clientPositionStatus);
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
 
-	@PostMapping("/addConsultantStatus")
-	public ResponseEntity<Void> addConsultantStatus(@RequestBody ConsultantStatus consultantStatus,
-			UriComponentsBuilder builder) {
-		consultantStatusRepository.save(consultantStatus);
+	@PostMapping("addClientConsultantStatus")
+	public ResponseEntity<Void> addConsultantStatus(@RequestBody ConsultantStatus consultantStatus) {
+		consultantStatusService.addConsultantStatus(consultantStatus);
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
 
