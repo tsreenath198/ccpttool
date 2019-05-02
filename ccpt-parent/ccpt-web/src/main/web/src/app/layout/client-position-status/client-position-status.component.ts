ab60db15905732e21f59f5fd2a09034cbce6b00f
snehith5949 <48270356+snehith5949@users.noreply.github.com>
2019-05-02 18:15:42 +0530

    modified client position status

--- ccpt-parent/ccpt-web/src/main/web/src/app/layout/client-position-status/client-position-status.component.ts
+++ ccpt-parent/ccpt-web/src/main/web/src/app/layout/client-position-status/client-position-status.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { ClientpositionStatusModel } from './client-position-status.model';
 import { HttpClientService } from 'src/app/shared/services/http.service';
+import { ToastrCustomService } from 'src/app/shared/services/toastr.service';
 
 @Component({
     selector: 'app-client-position-status',
@@ -10,21 +11,31 @@ import { HttpClientService } from 'src/app/shared/services/http.service';
     animations: [routerTransition()]
 })
 export class ClientPositionStatusComponent implements OnInit {
-    public clientPositionStatusModel:ClientpositionStatusModel = <ClientpositionStatusModel>{};
-    public clientPositionStatusList:Array<ClientpositionStatusModel> = [];
+    public clientPositionStatusModel: ClientpositionStatusModel = <ClientpositionStatusModel>{};
+    public clientPositionStatusList: Array<ClientpositionStatusModel> = [];
 
-    constructor(private http: HttpClientService) { }
+    constructor(private http: HttpClientService, private toastr: ToastrCustomService) { }
     ngOnInit() {
+        this.init();
+    }
+    init() {
         this.http.get('admin/getAllClientPositionStatus').subscribe(resp => {
             this.clientPositionStatusList = resp as [];
         })
     }
+    formReset() {
+        this.clientPositionStatusModel = <ClientpositionStatusModel>{};
+    }
     submit(): void {
         this.http.create(this.clientPositionStatusModel, 'admin/addClientPositionStatus').subscribe(resp => {
-
-
+            this.toastr.success("Form Submitted Successfully", "Client Position Status");
+            this.init();
+            this.formReset();
+        }, err => {
+            this.toastr.error(err.statusText, "Client Position Status");
         })
-        this.clientPositionStatusModel = <ClientpositionStatusModel>{};
+
+
     }
 }
 
