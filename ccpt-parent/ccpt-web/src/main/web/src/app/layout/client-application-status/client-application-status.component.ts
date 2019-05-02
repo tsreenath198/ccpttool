9a194718e29445ad859544ed422815842263e28e
snehith5949 <48270356+snehith5949@users.noreply.github.com>
2019-05-02 18:14:20 +0530

    Modified client application status component

--- ccpt-parent/ccpt-web/src/main/web/src/app/layout/client-application-status/client-application-status.component.ts
+++ ccpt-parent/ccpt-web/src/main/web/src/app/layout/client-application-status/client-application-status.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { ClientApplicationStatusModel } from './client-application-status.model';
 import { HttpClientService } from 'src/app/shared/services/http.service';
+import { ToastrCustomService } from 'src/app/shared/services/toastr.service';
 
 @Component({
     selector: 'app-client-application-status',
@@ -12,17 +13,27 @@ import { HttpClientService } from 'src/app/shared/services/http.service';
 export class ClientApplicationStatusComponent implements OnInit {
     public clientApplicationStatusModel:ClientApplicationStatusModel = <ClientApplicationStatusModel>{};
     public clientApplicationStatusList:Array<ClientApplicationStatusModel>=[];
-    constructor(private http: HttpClientService) { }
+    constructor(private http: HttpClientService,private toastr: ToastrCustomService) { }
     
 
     ngOnInit() {
+        this.init();
+    }
+    init(){
         this.http.get('admin/getAllClientApplicationStatus').subscribe(resp => {
             this.clientApplicationStatusList = resp as [];
         })
     }
+    formReset(){
+        this.clientApplicationStatusModel = <ClientApplicationStatusModel>{};
+    }
     submit(): void {
         this.http.create(this.clientApplicationStatusModel, 'admin/addClientApplicationStatus').subscribe(resp => {
-        })
-        this.clientApplicationStatusModel = <ClientApplicationStatusModel>{};
+            this.toastr.success("Form Submitted Successfully", "Client Application Status");
+            this.init();
+            this.formReset();
+        }, err => {
+            this.toastr.error(err.statusText, "Client Application Status");
+        });
     }
 }
