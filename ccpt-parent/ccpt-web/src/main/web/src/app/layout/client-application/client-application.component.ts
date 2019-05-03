afc4c04ebecd316a5beb3268c764dabd96a609ec
snehith5949 <48270356+snehith5949@users.noreply.github.com>
2019-05-03 12:33:28 +0530

    updated client application (buttons toggle, read only when clicked on table data)

--- ccpt-parent/ccpt-web/src/main/web/src/app/layout/client-application/client-application.component.ts
+++ ccpt-parent/ccpt-web/src/main/web/src/app/layout/client-application/client-application.component.ts
@@ -19,14 +19,15 @@ export class ClientApplicationComponent implements OnInit {
     public consultantList:Array<ConsultantModel> = [];
     public clientApplicationStatusList:Array<ClientApplicationStatusModel> = [];
     public clientPositionList:Array<ClientPositionModel>=[];
+    public readOnlyToggler :boolean=false;
+    public formButtonsToggler :boolean=true;
+    public editButtonToggler:boolean=true;
     constructor(private http: HttpClientService , private toastr: ToastrCustomService) { }
 
     ngOnInit() {
+        this.init();
         this.http.get('admin/getAllClientApplicationStatus').subscribe(resp => {
             this.clientApplicationStatusList = resp as [];
-        })
-        this.http.get('clientApplication/getAll').subscribe(resp => {
-            this.clientApplicationList = resp as [];
         })
         this.http.get('consultant/getAll').subscribe(resp => {
             this.consultantList = resp as [];
@@ -35,31 +36,82 @@ export class ClientApplicationComponent implements OnInit {
             this.clientPositionList = resp as [];
         })
     }
+    init(){
+        this.http.get('clientApplication/getAll').subscribe(resp => {
+            this.clientApplicationList = resp as [];
+        })
+    }
     
     formReset(){
         this.clientApplicationModel = <ClientApplicationModel>{};
     }
+    readOnlyEnable(data){
+        this.clientApplicationModel = data;
+        if(this.readOnlyToggler==false){
+            this.readOnlyToggler=true;
+        }
+        if(this.formButtonsToggler==true){
+            this.formButtonsToggler=false;
+        }
+    }
     submit(): void {
         this.http.create(this.clientApplicationModel, 'clientApplication/create').subscribe(resp => {
             this.toastr.success("Form Submitted Successfully", "Client Application");
+            this.init();
+            this.formReset()
+
+        }, err => {
+            this.toastr.error(err.statusText, "Client Application");
         })
         
     }
     editClientApplication(data){
         this.clientApplicationModel=data;
+        if(this.readOnlyToggler==true){
+            this.readOnlyToggler=false;
+        }
+        if(this.formButtonsToggler==true){
+            this.formButtonsToggler=false;
+        }
+        if(this.editButtonToggler==true){
+            this.editButtonToggler=false;
+        }
     }
     updateClientApplication(){
         this.http.update(this.clientApplicationModel, 'clientApplication/update').subscribe(resp => {
-
-
+            this.toastr.success("Form Updated Successfully", "Client Application");
+            this.init();
+            this.editButtonToggler=true;
+        }, err => {
+            this.toastr.error(err.statusText, "Client Application");
         })
         this.formReset();
     }
     deleteClientApplication() {
         this.http.delete('clientApplication/id/' + this.clientApplicationModel.id).subscribe(resp => {
-
+            this.toastr.success("Form Deleted Successfully", "Client Application");
+            this.init();
+            this.formReset();
 
         })
         this.formReset();
+    }
+    editableForm(){
+        if(this.readOnlyToggler==true){
+            this.readOnlyToggler=false;
+        }
+        if(this.editButtonToggler==true){
+            this.editButtonToggler=false;
+        }
+    }
+    cancelForm(){
+        this.formReset();
+        if(this.readOnlyToggler==true){
+            this.readOnlyToggler=false;
+        }
+        if(this.formButtonsToggler==false){
+            this.formButtonsToggler=true;
+        }
+        
     }
 }
