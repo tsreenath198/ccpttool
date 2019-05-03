f44bebadcd50f243cc06802b1674700b1d653363
snehith5949 <48270356+snehith5949@users.noreply.github.com>
2019-05-03 14:11:06 +0530

    modified recruiter component(readonly when selected from table and toggling update edit delete buttons)

--- ccpt-parent/ccpt-web/src/main/web/src/app/layout/recruiter/recruiter.component.ts
+++ ccpt-parent/ccpt-web/src/main/web/src/app/layout/recruiter/recruiter.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { RecruiterModel } from './recruiter.model';
 import { HttpClientService } from 'src/app/shared/services/http.service';
+import { ToastrCustomService } from 'src/app/shared/services/toastr.service';
 
 @Component({
     selector: 'app-recruiter',
@@ -11,34 +12,85 @@ import { HttpClientService } from 'src/app/shared/services/http.service';
 })
 export class RecruiterComponent implements OnInit {
     public recruiterModel:RecruiterModel = <RecruiterModel>{};
+    public readOnlyToggler :boolean=false;
+    public formButtonsToggler :boolean=true;
+    public editButtonToggler:boolean=true;
     public recruiterList:Array<RecruiterModel> = [];
 
-    constructor(private http: HttpClientService) { }
+    constructor(private http: HttpClientService,private toastr: ToastrCustomService) { }
     ngOnInit() {
+        this.init();
+    }
+    init(){
         this.http.get('recruiter/getAll').subscribe(resp => {
             this.recruiterList = resp as [];
         })
     }
-    edit(data){
+    recruiterEdit(data){
         this.recruiterModel=data;
+        if(this.readOnlyToggler==true){
+            this.readOnlyToggler=false;
+        }
+        if(this.formButtonsToggler==true){
+            this.formButtonsToggler=false;
+        }
+        if(this.editButtonToggler==true){
+            this.editButtonToggler=false;
+        }
+    }
+    readOnlyEnable(data){
+        this.recruiterModel = data;
+        if(this.readOnlyToggler==false){
+            this.readOnlyToggler=true;
+        }
+        if(this.formButtonsToggler==true){
+            this.formButtonsToggler=false;
+        }
+    }
+    formReset() {
+        this.recruiterModel = <RecruiterModel>{};
     }
     submit(): void {
         this.http.create(this.recruiterModel, 'recruiter/create').subscribe(resp => {
-
-
+            this.toastr.success("Form Submitted Successfully", "Recruiter");
+            this.init();
+            this.formReset();
+        }, err => {
+            this.toastr.error(err.statusText, "Recruiter");
         })
-        this.recruiterModel = <RecruiterModel>{};
     }
     update(){
         this.http.update(this.recruiterModel, 'recruiter/update').subscribe(resp => {
-
-
+            this.toastr.success("Form Updated Successfully", "Recruiter");
+            this.init();
+            this.editButtonToggler=true;
+        }, err => {
+            this.toastr.error(err.statusText, "Recruiter");
         })
-        this.recruiterModel = <RecruiterModel>{};
     }
     delete() {
         this.http.delete('recruiter/id/' + this.recruiterModel.id).subscribe(resp => {
-
+            this.toastr.success("Form Deleted Successfully", "Recruiter");
+            this.init();
+            this.formReset();
         })
+    }
+    editableForm(){
+        if(this.readOnlyToggler==true){
+            this.readOnlyToggler=false;
+        }
+        if(this.editButtonToggler==true){
+            this.editButtonToggler=false;
+        }
+    }
+    cancelForm(){
+        this.formReset();
+        if(this.readOnlyToggler==true){
+            this.readOnlyToggler=false;
+        }
+        if(this.formButtonsToggler==false){
+            this.formButtonsToggler=true;
+        }
+        
     }
 }
