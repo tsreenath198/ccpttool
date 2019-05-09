5a8ddcdc84a908a54c7142967aed9ee0df41459d
snehith5949 <48270356+snehith5949@users.noreply.github.com>
2019-05-09 17:52:28 +0530

    added required fields and pending work on controller(backend)

--- ccpt-parent/ccpt-web/src/main/web/src/app/layout/login-setup/login-setup.component.ts
+++ ccpt-parent/ccpt-web/src/main/web/src/app/layout/login-setup/login-setup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { LoginSetupModel } from './login-setup.model';
 import { HttpClientService } from 'src/app/shared/services/http.service';
+import { URLConstants } from '../components/constants/url-constants';
 
 @Component({
     selector: 'app-login-setup',
@@ -12,19 +13,23 @@ import { HttpClientService } from 'src/app/shared/services/http.service';
 export class LoginSetupComponent implements OnInit {
     public loginSetupModel:LoginSetupModel = <LoginSetupModel>{};
     public loginSetupList:Array<LoginSetupModel> = [];
+    private urlConstants = new URLConstants();
+    private getAllR:any;
 
     constructor(private http: HttpClientService) { }
 
     ngOnInit() {
-        // this.http.get('consultant/getAll').subscribe(resp => {
-        //     this.loginSetupList = resp as [];
-        // })
+         this.http.get(this.urlConstants.RGetAll).subscribe(resp => {
+         this.getAllR = resp as any;
+     })
+
     }
     submit(): void {
-        this.http.create(this.loginSetupModel, 'url').subscribe(resp => {
+        console.log(this.loginSetupModel)
+        // this.http.create(this.loginSetupModel, 'url').subscribe(resp => {
 
 
-        })
+        // })
         this.loginSetupModel = <LoginSetupModel>{};
     }
 }
