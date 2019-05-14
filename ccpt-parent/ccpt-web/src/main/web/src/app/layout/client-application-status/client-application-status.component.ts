24deac8ec0652aacf0cb4af33e707abbb412b94a
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2019-05-14 17:58:29 +0530

    With all required functionalities done this page

--- ccpt-parent/ccpt-web/src/main/web/src/app/layout/client-application-status/client-application-status.component.ts
+++ ccpt-parent/ccpt-web/src/main/web/src/app/layout/client-application-status/client-application-status.component.ts
@@ -4,6 +4,7 @@ import { URLConstants } from '../components/constants/url-constants';
 import { ClientApplicationStatusModel } from './client-application-status.model';
 import { HttpClientService } from 'src/app/shared/services/http.service';
 import { ToastrCustomService } from 'src/app/shared/services/toastr.service';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { NgForm } from '@angular/forms';
 
 
@@ -17,11 +18,11 @@ export class ClientApplicationStatusComponent implements OnInit {
     public clientApplicationStatusModel: ClientApplicationStatusModel = <ClientApplicationStatusModel>{};
     public clientApplicationStatusList: Array<ClientApplicationStatusModel> = [];
     private urlConstants = new URLConstants();
-    public readOnlyForm :boolean=false;
-    public formButtonsToggler :boolean=true;
-    public editButtonToggler:boolean=true;
-    public currSearchTxt: string ;
-    constructor(private http: HttpClientService, private toastr: ToastrCustomService) {
+    public readOnlyForm: string = '';
+    public enableButtonType: string = '';
+    public currSearchTxt: string;
+    closeResult: string;
+    constructor(private http: HttpClientService, private toastr: ToastrCustomService,private modalService: NgbModal) {
     }
     ngOnInit() {
         this.init();
@@ -31,31 +32,25 @@ export class ClientApplicationStatusComponent implements OnInit {
             this.clientApplicationStatusList = resp as any;
         })
     }
-    editClientApplicationStatus(data) {
-        this.clientApplicationStatusModel = data;
-        if(this.readOnlyForm==true){
-            this.readOnlyForm=false;
-        }
-        if(this.formButtonsToggler==true){
-            this.formButtonsToggler=false;
-        }
-        if(this.editButtonToggler==true){
-            this.editButtonToggler=false;
-        }
+    editClientApplicationStatus(data): void {
+        this.clientApplicationStatusModel = JSON.parse(JSON.stringify(data));
+        this.readOnlyForm = 'U';
+        this.enableButtonType = 'U';
     }
-    readOnlyEnable(data){
-        this.clientApplicationStatusModel = data;
-        if(this.readOnlyForm==false){
-            this.readOnlyForm=true;
-        }
-        if(this.formButtonsToggler==true){
-            this.formButtonsToggler=false;
-        }
+    enableFormEditable(): void {
+        this.readOnlyForm = 'U';
+        this.enableButtonType = 'U';
     }
-    formReset() {
+
+    readOnlyEnable(data): void {
+        this.clientApplicationStatusModel = JSON.parse(JSON.stringify(data));
+        this.readOnlyForm = 'R';
+        this.enableButtonType = 'E';
+    }
+    formReset(): void {
         this.clientApplicationStatusModel = <ClientApplicationStatusModel>{};
     }
-    createClientApplicationStatus(clientApplicationStatusForm:NgForm): void {
+    createClientApplicationStatus(clientApplicationStatusForm: NgForm): void {
         this.http.create(this.clientApplicationStatusModel, this.urlConstants.CASCreate).subscribe(resp => {
             this.toastr.success("Form Submitted Successfully", "Client Application Status");
             this.init();
@@ -65,46 +60,49 @@ export class ClientApplicationStatusComponent implements OnInit {
             this.toastr.error(err.statusText, "Client Application Status");
         });
     }
-    updateClientApplicationStatus(clientApplicationStatusForm:NgForm) {
+    updateClientApplicationStatus(clientApplicationStatusForm: NgForm): void {
         this.http.update(this.clientApplicationStatusModel, this.urlConstants.CASUpdate).subscribe(resp => {
             this.toastr.success("Form Updated Successfully", "Client Application Status");
-            this.formButtonsToggler = true;
             this.formReset();
             this.init();
             clientApplicationStatusForm.resetForm();
+            this.readOnlyForm = '';
+            this.enableButtonType = '';
         }, err => {
             this.toastr.error(err.statusText, "Client Application Status");
         })
     }
-    deleteClientApplicationStatus(deleteId) {
+    deleteClientApplicationStatus(deleteId): void {
         this.http.delete(this.urlConstants.CASDelete + deleteId).subscribe(resp => {
             this.toastr.success("Form Deleted Successfully", "Client Application Status");
             this.init();
             this.formReset();
         })
     }
-    editableForm(){
-        if(this.readOnlyForm==true){
-            this.readOnlyForm=false;
-        }
-        if(this.editButtonToggler==true){
-            this.editButtonToggler=false;
-        }
-    }
-    cancelForm(clientApplicationStatusForm:NgForm){
+    cancelForm(clientApplicationStatusForm: NgForm): void {
         this.formReset();
         clientApplicationStatusForm.resetForm();
-        if(this.readOnlyForm==true){
-            this.readOnlyForm=false;
-        }
-        if(this.formButtonsToggler==false){
-            this.formButtonsToggler=true;
-        }
-        
+        this.readOnlyForm = '';
+        this.enableButtonType = '';
     }
-    deleteConfirmation(toDelete){
-        if (confirm("Are you sure you want to delete the row!")) {
+    deleteCASRecord(toDelete): void {
             this.deleteClientApplicationStatus(toDelete.code);
-          } 
+    }
+
+    open(content) {
+        this.modalService.open(content).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return  `with: ${reason}`;
+        }
     }
 }
--- ccpt-parent/ccpt-web/src/main/web/src/app/layout/client-application-status/client-application-status.module.ts
+++ ccpt-parent/ccpt-web/src/main/web/src/app/layout/client-application-status/client-application-status.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import  {FormsModule} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { PageHeaderModule, SharedPipesModule } from '../../shared';
 import { ClientApplicationStatusComponent } from './client-application-status.component';
 import { ClientApplicationStatusRoutingModule } from './client-application-status-routing.module';
 
 @NgModule({
-    imports: [CommonModule, ClientApplicationStatusRoutingModule,FormsModule, PageHeaderModule,SharedPipesModule],
+    imports: [CommonModule, ClientApplicationStatusRoutingModule,FormsModule, PageHeaderModule,NgbModule,SharedPipesModule],
     declarations: [ClientApplicationStatusComponent]
 })
 export class ClientApplicationStatusModule {}
