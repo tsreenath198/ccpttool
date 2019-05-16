f093dce9cd9b156222cc4a408077e76716935e36
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2019-05-16 15:59:51 +0530

    Modal for delete

--- ccpt-parent/ccpt-web/src/main/web/src/app/layout/client-call-history/client-call-history.component.ts
+++ ccpt-parent/ccpt-web/src/main/web/src/app/layout/client-call-history/client-call-history.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { ClientCallHistoryModel } from './client-call-history.model';
 import { HttpClientService } from 'src/app/shared/services/http.service';
 import { ClientpositionStatusModel } from '../client-position-status/client-position-status.model';
@@ -25,8 +26,11 @@ export class ClientCallHistoryComponent implements OnInit {
     public formButtonsToggler: boolean = true;
     public editButtonToggler: boolean = true;
     public urlConstants = new URLConstants();
+    private selectedRecrdToDel: number = 0;
+    public closeResult: string = '';
+    private modalRef: NgbModalRef;
     public currSearchTxt: string;
-    constructor(private http: HttpClientService, private toastr: ToastrCustomService) { }
+    constructor(private http: HttpClientService, private toastr: ToastrCustomService, private modalService: NgbModal) { }
 
     ngOnInit() {
         this.http.get(this.urlConstants.CPGetAll).subscribe(resp => {
@@ -88,13 +92,6 @@ export class ClientCallHistoryComponent implements OnInit {
             this.toastr.error(err.statusText, "Client Call History");
         })
     }
-    deleteClientCallHistory(deleteId) {
-        this.http.delete('clientCallHistory/id/' + deleteId).subscribe(resp => {
-            this.toastr.success("Form Deleted Successfully", "Client Call History");
-            this.init();
-            this.formReset();
-        })
-    }
     editableForm() {
         if (this.readOnlyForm == true) {
             this.readOnlyForm = false;
@@ -114,9 +111,40 @@ export class ClientCallHistoryComponent implements OnInit {
         }
 
     }
-    deleteConfirmation(toDelete) {
-        if (confirm("Are you sure you want to delete the row!")) {
-            this.deleteClientCallHistory(toDelete.id);
+    deleteCCHRecord(): void {
+        this.http.delete(this.urlConstants.CCHDelete + this.selectedRecrdToDel).subscribe(resp => {
+            this.toastr.success("Form Deleted Successfully", "Client");
+            this.init();
+            this.close();
+            this.formReset();
+        })
+    }
+    /**
+     * @param 
+     * 1) content consists the modal instance
+     * 2) Selected contains the code of selected row
+     */
+    open(content, selected: number) {
+        if (selected) {
+            this.selectedRecrdToDel = selected;
+        }
+        this.modalRef = this.modalService.open(content);
+        this.modalRef.result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+    close() {
+        this.modalRef.close();
+    }
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
         }
     }
 }
