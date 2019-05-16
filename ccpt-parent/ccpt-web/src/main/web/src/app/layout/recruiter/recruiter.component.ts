2a4a2c3ff9a0cf62700b1883267e6bc03b466f1c
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2019-05-16 15:57:45 +0530

    Modal for delete

--- ccpt-parent/ccpt-web/src/main/web/src/app/layout/recruiter/recruiter.component.ts
+++ ccpt-parent/ccpt-web/src/main/web/src/app/layout/recruiter/recruiter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { RecruiterModel, Roles } from './recruiter.model';
 import { HttpClientService } from 'src/app/shared/services/http.service';
 import { ToastrCustomService } from 'src/app/shared/services/toastr.service';
@@ -21,10 +22,13 @@ export class RecruiterComponent implements OnInit {
     public rolesModel = new Roles();
     public rolesList: any = [];
     public urlConstants = new URLConstants();
+    private selectedRecrdToDel: number = 0;
+    public closeResult: string = '';
+    private modalRef: NgbModalRef;
     public genderList=['MALE','FEMALE','OTHER'];
     public currSearchTxt: string ;
 
-    constructor(private http: HttpClientService, private toastr: ToastrCustomService) { }
+    constructor(private http: HttpClientService, private toastr: ToastrCustomService, private modalService: NgbModal) { }
     ngOnInit() {
         this.init();
         this.rolesList = this.rolesModel.roles;
@@ -60,7 +64,7 @@ export class RecruiterComponent implements OnInit {
     }
     createRecruiter(recruiterForm:NgForm): void {
         this.http.create(this.recruiterModel, this.urlConstants.RCreate).subscribe(resp => {
-            this.toastr.success("Form Submitted Successfully", "Recruiter");
+            this.toastr.success(this.urlConstants.SuccessMsg, "Recruiter");
             this.init();
             this.formReset();
             recruiterForm.resetForm();
@@ -70,20 +74,13 @@ export class RecruiterComponent implements OnInit {
     }
     updateRecruiter(recruiterForm:NgForm) {
         this.http.update(this.recruiterModel, this.urlConstants.RUpdate).subscribe(resp => {
-            this.toastr.success("Form Updated Successfully", "Recruiter");
+            this.toastr.success(this.urlConstants.UpdateMsg, "Recruiter");
             this.init();
             this.formButtonsToggler = true;
             this.formReset();
             recruiterForm.resetForm();
         }, err => {
             this.toastr.error(err.statusText, "Recruiter");
-        })
-    }
-    deleteRecruiter(deleteId) {
-        this.http.delete(this.urlConstants.RDelete + deleteId).subscribe(resp => {
-            this.toastr.success("Form Deleted Successfully", "Recruiter");
-            this.init();
-            this.formReset();
         })
     }
     editableForm() {
@@ -103,12 +100,42 @@ export class RecruiterComponent implements OnInit {
         if (this.formButtonsToggler == false) {
             this.formButtonsToggler = true;
         }
-
+    }
+    deleteRecruiterRecord(): void {
+        this.http.delete(this.urlConstants.RDelete + this.selectedRecrdToDel).subscribe(resp => {
+            this.toastr.success(this.urlConstants.DeleteMsg, "Recruiter");
+            this.init();
+            this.close();
+            this.formReset();
+        })
+    }
+    /**
+     * @param 
+     * 1) content consists the modal instance
+     * 2) Selected contains the code of selected row
+     */
+    open(content, selected: number) {
+        if (selected) {
+            this.selectedRecrdToDel = selected;
+        }
+        this.modalRef = this.modalService.open(content);
+        this.modalRef.result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+    close() {
+        this.modalRef.close();
+    }
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
+    }
     
-    }
-    deleteConfirmation(toDelete){
-        if (confirm("Are you sure you want to delete the row!")) {
-            this.deleteRecruiter(toDelete.id);
-          } 
-    }
 }
