f99d8c2001e14f38420986f69c64e7145a664ad5
pavankumar2107 <36192034+pavankumar2107@users.noreply.github.com>
2019-05-20 17:13:26 +0530

    backend changes

--- ccpt-parent/ccpt-server/src/main/java/com/ccpt/repository/RecruiterRepository.java
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/repository/RecruiterRepository.java
@@ -8,10 +8,11 @@ import com.ccpt.model.Recruiter;
 
 public interface RecruiterRepository extends CrudRepository<Recruiter, Integer> {
 	public List<Recruiter> findAllByOrderByUpdatedDateDesc();
-//	public List<Recruiter> findByStatus(String status);
-	
-	 List<Recruiter> findByActiveFlagAndStatusAllIgnoreCaseOrderByUpdatedDateDesc(String activeFlag, String status);
-	 
-	 List<Recruiter> findByActiveFlagAllIgnoreCaseOrderByUpdatedDateDesc(String ActiveFlag);
+	// public List<Recruiter> findByStatus(String status);
+
+	List<Recruiter> findByActiveFlagAndStatusAllIgnoreCaseOrderByUpdatedDateDesc(String activeFlag, String status);
+
+	List<Recruiter> findByActiveFlagAllIgnoreCaseOrderByUpdatedDateDesc(String ActiveFlag);
+
 	Recruiter findByIdAndActiveFlag(int id, Character status);
 }
