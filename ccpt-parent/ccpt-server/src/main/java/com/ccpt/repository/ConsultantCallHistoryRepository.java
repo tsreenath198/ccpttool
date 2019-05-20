7d7f9eaa4bd34d87b2778d53437c72dd84ed664b
pavankumar2107 <36192034+pavankumar2107@users.noreply.github.com>
2019-05-20 12:48:37 +0530

    backend changes

--- ccpt-parent/ccpt-server/src/main/java/com/ccpt/repository/ConsultantCallHistoryRepository.java
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/repository/ConsultantCallHistoryRepository.java
@@ -28,7 +28,7 @@ public interface ConsultantCallHistoryRepository extends CrudRepository<Consulta
 
 	List<ConsultantCallHistory> findByActiveFlagAllIgnoreCaseOrderByUpdatedDateDesc(String ActiveFlag);
 
-	@Query("SELECT  r.fullname, count(*) FROM ClientApplication ca left outer join Recruiter r on ca.closedBy = r.id where ca.closedBy is not null group by r.id, r.fullname ")
+	@Query("SELECT  r.fullname, count(*) FROM ClientApplication ca left outer join Recruiter r on ca.closedBy = r.id where ca.closedBy is not null AND (ca.createdDate   BETWEEN  :sdate AND  :edate) group by r.id, r.fullname ")
 	List<Object[]> getClosedCountOfAllRecruitersFromLastGivenDays(@Param(value = "sdate") Date sdate,
 			@Param(value = "edate") Date edate);
 
