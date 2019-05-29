92be6caa69b41cb1b593c714337db9214b8c62fc
Sreenath <Sreenath@DESKTOP-VBIK0HI>
2019-05-29 13:25:44 +0530

    sms changes

--- ccpt-parent/ccpt-server/src/main/java/com/ccpt/controller/SMSController.java
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/controller/SMSController.java
@@ -3,6 +3,7 @@ package com.ccpt.controller;
 import java.net.URISyntaxException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -25,21 +26,32 @@ import com.ccpt.model.SMS;
 @RequestMapping(CCPTConstants.SMS)
 public class SMSController {
 
+	@Value("${sms.url}")
+	private String url;
+
+	@Value("${sms.auth.key}")
+	private String authKey;
+
+	@Value("${sms.sender}")
+	private String sender;
+
+	@Value("${sms.route}")
+	private String route;
+
 	@PostMapping(CCPTConstants.SEND)
 	public ResponseEntity<String> sendSMS(@RequestBody SMS sms) throws URISyntaxException {
 		List<String> contactNumbers = sms.getContactNumbers();
 		String message = sms.getMessage();
 		RestTemplate restTemplate = new RestTemplate();
 		String contactsCommaSeparated = String.join(",", contactNumbers);
-		final String url = "http://sms.bulksmsserviceproviders.com/api/send_http.php";
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
 		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-		map.add("authkey", "7dc7c47175bbfd1b0b3286ee0e42acdd");
-		map.add("sender", "TLTCNR");
-		map.add("route", "4");
+		map.add("authkey", authKey);
+		map.add("sender", sender);
+		map.add("route", route);
 		map.add("message", message);
 		map.add("mobiles", contactsCommaSeparated);
 
