7167c6a83b9976b9805daf4927ce0e17f0e9f109
snehith5949 <48270356+snehith5949@users.noreply.github.com>
2019-06-07 09:56:11 +0530

    update and delete

--- ccpt-parent/ccpt-web/src/main/web/src/app/layout/users/users.component.ts
+++ ccpt-parent/ccpt-web/src/main/web/src/app/layout/users/users.component.ts
@@ -5,6 +5,7 @@ import { HttpClientService } from 'src/app/shared/services/http.service';
 import { URLConstants } from '../components/constants/url-constants';
 import { NgForm } from '@angular/forms';
 import { ToastrCustomService } from 'src/app/shared/services/toastr.service';
+import { NgbModalRef, ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-users',
@@ -17,8 +18,14 @@ export class UsersComponent implements OnInit {
     public usersList: Array<UsersModel> = [];
     private urlConstants = new URLConstants();
     public getAllR: any;
+    public readOnlyForm = '';
+    public enableButtonType = '';
+    public currSearchTxt = '';
+    private selectedRecrdToDel: number = 0;
+    public closeResult: string = '';
+    private modalRef: NgbModalRef;
 
-    constructor(private http: HttpClientService, private toastr: ToastrCustomService) { }
+    constructor(private http: HttpClientService, private toastr: ToastrCustomService,private modalService: NgbModal) { }
 
     ngOnInit() {
         this.http.get(this.urlConstants.RGetAll).subscribe(resp => {
@@ -27,21 +34,86 @@ export class UsersComponent implements OnInit {
         this.init();
 
     }
-    init(){
+    init() {
         this.http.get(this.urlConstants.UserGetAll).subscribe(resp => {
             this.usersList = resp as any;
         });
     }
+    editUser(data) {
+        this.usersModel = JSON.parse(JSON.stringify(data));
+        this.readOnlyForm = 'U';
+        this.enableButtonType = 'U';
+    }
+    enableFormEditable(): void {
+        this.readOnlyForm = 'U';
+        this.enableButtonType = 'U';
+    }
+    readOnlyEnable(data) {
+        this.usersModel = JSON.parse(JSON.stringify(data));
+        this.readOnlyForm = 'R';
+        this.enableButtonType = 'E';
+    }
+    formReset() {
+        this.usersModel = <UsersModel>{};
+    }
     createUser(usersForm: NgForm): void {
         this.http.post(this.usersModel, this.urlConstants.UserCreate).subscribe(resp => {
-            this.toastr.success(this.urlConstants.SuccessMsg, 'Contact');
+            this.toastr.success(this.urlConstants.SuccessMsg, 'User');
             usersForm.resetForm();
             this.init();
         }, err => {
             this.toastr.error(err.statusText, 'Users');
         });
     }
+    updateUsers(usersForm: NgForm): void {
+        this.http.update(this.usersModel, this.urlConstants.UserUpdate).subscribe(resp => {
+            this.formReset();
+            this.toastr.success(this.urlConstants.UpdateMsg, 'User');
+            this.init();
+            usersForm.resetForm();
+            this.readOnlyForm = '';
+            this.enableButtonType = '';
+        }, err => {
+            this.toastr.error(err.statusText, 'User' );
+        });
+    }
+    deleteClientRecord(): void {
+        this.http.delete(this.urlConstants.OCDelete + this.selectedRecrdToDel).subscribe(resp => {
+            this.toastr.success(this.urlConstants.DeleteMsg, "Client");
+            this.init();
+            this.close();
+            this.formReset();
+        })
+    }
     cancelForm(usersForm: NgForm) {
         usersForm.resetForm();
+    }
+     /**
+     * @param 
+     * 1) content consists the modal instance
+     * 2) Selected contains the code of selected row
+     */
+    open(content, selected: number) {
+        if (selected) {
+            this.selectedRecrdToDel = selected;
+        }
+        this.modalRef = this.modalService.open(content);
+        this.modalRef.result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+    close() {
+        this.modalRef.close();
+    }
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
     }
 }
