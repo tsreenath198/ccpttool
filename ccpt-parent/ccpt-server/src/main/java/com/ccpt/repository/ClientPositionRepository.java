2b58f958472918ec6815e48d4ea229524f8e76eb
pavankumar2107 <36192034+pavankumar2107@users.noreply.github.com>
2019-06-24 10:47:04 +0530

    backend changes

--- ccpt-parent/ccpt-server/src/main/java/com/ccpt/repository/ClientPositionRepository.java
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/repository/ClientPositionRepository.java
@@ -20,7 +20,7 @@ public interface ClientPositionRepository extends BaseRepository<ClientPosition,
 	List<ClientPosition> findByClientIdAndActiveFlag(@Param("clientId") Integer clientId,
 			@Param("activeFlag") Boolean activeFlag);
 
-	@Query("SELECT id as id,generatedCode as name FROM ClientPosition WHERE active_flag=1")
+	@Query("SELECT id as id,generatedCode as name FROM ClientPosition WHERE active_flag=1 AND status_code != 'Closed'")
 	List<DropDownStatistics> getAllCps();
 
 }
