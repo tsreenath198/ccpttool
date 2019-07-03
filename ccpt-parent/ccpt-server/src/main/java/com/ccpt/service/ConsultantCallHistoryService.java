0eb4239a6a7399eda2f0ef57805e1e4d37ec0c57
anuraguskcorp <anurag.uskcorp@gmail.com>
2019-07-03 13:48:49 +0530

    Consultant call history by recruiter by days

--- ccpt-parent/ccpt-server/src/main/java/com/ccpt/service/ConsultantCallHistoryService.java
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/service/ConsultantCallHistoryService.java
@@ -1,7 +1,13 @@
 package com.ccpt.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,13 +28,14 @@ public class ConsultantCallHistoryService extends BaseService<ConsultantCallHist
 	@Autowired
 	private ConsultantCallHistoryRepository consultantCallHistoryRepository;
 
-	public List<ConsultantCallHistory> getAllConsultantCallHistorysFromLastGivenDays(Integer days) {
+	public List<ConsultantCallHistory> getAllConsultantCallHistorysFromLastGivenDays(Integer days)
+			throws ParseException {
 		Day day = getDays(days);
 		return consultantCallHistoryRepository.getAllConsultantCallHistorysFromLastGivenDays(day.getStartDate(),
 				day.getEndDate(), true);
 	}
 
-	public List<Object[]> getClosedCountOfAllRecruitersFromLastGivenDays(Integer days) {
+	public List<Object[]> getClosedCountOfAllRecruitersFromLastGivenDays(Integer days) throws ParseException {
 		Day day = getDays(days);
 		return consultantCallHistoryRepository.getClosedCountOfAllRecruitersFromLastGivenDays(day.getStartDate(),
 				day.getEndDate());
@@ -39,23 +46,55 @@ public class ConsultantCallHistoryService extends BaseService<ConsultantCallHist
 		return consultantCallHistoryRepository;
 	}
 
-	public List<CallHistorySummaryStatistics> getAllconCHCountByRecruiters(Integer days) {
+	public List<CallHistorySummaryStatistics> getAllconCHCountByRecruiters(Integer days) throws ParseException {
 		Day day = getDays(days);
 		return consultantCallHistoryRepository.getAllconCHCountByRecruiters(day.getStartDate(), day.getEndDate());
 	}
 
-	public List<ConsultantCallHistory> getAllconCHByRecruiterId(Integer rId, Integer days) {
+	public List<ConsultantCallHistory> getAllconCHByRecruiterId(Integer rId, Integer days) throws ParseException {
 		Day day = getDays(days);
 		return consultantCallHistoryRepository.getAllconCHByRecruiterId(rId, day.getStartDate(), day.getEndDate(),
 				true);
 	}
 
-	public Day getDays(Integer days) {
+	public Day getDays(Integer days) throws ParseException {
 		Day day = new Day();
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DATE, -days);
-		day.setStartDate(cal.getTime());
+		LocalDate localDate = LocalDate.now();
+		if (days == 1) {
+			java.util.Date utilDate = calenderToDate();
+			day.setStartDate(utilDate);
+		}
+		// 2019-07-01
+		if (days == 7) {
+			Date date = strToDate(localDate.with(TemporalAdjusters.previous(DayOfWeek.MONDAY)).toString());
+			day.setStartDate(date);
+		}
+		if (days == 30) {
+			Date date = strToDate(localDate.withDayOfMonth(1).toString());
+			day.setStartDate(date);
+		}
+		if (days == 365) {
+			Date date = strToDate(localDate.withDayOfYear(1).toString());
+			day.setStartDate(date);
+		}
 		day.setEndDate(new Date());
 		return day;
 	}
+
+	private Date strToDate(String dt) throws ParseException {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = formatter.parse(dt);
+		return date;
+	}
+
+	private Date calenderToDate() {
+		Calendar date = new GregorianCalendar();
+		date.set(Calendar.HOUR_OF_DAY, 0);
+		date.set(Calendar.MINUTE, 0);
+		date.set(Calendar.SECOND, 0);
+		date.set(Calendar.MILLISECOND, 0);
+		java.util.Date ud = date.getTime();
+		return ud;
+	}
+
 }
