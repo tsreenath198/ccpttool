0b3f673cfa124b163444c731c60fd9a54e05377a
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2019-07-04 16:36:03 +0530

    null check for body mail

--- ccpt-parent/ccpt-web/src/main/web/src/app/layout/consultant-call-history/consultant-call-history.module.ts
+++ ccpt-parent/ccpt-web/src/main/web/src/app/layout/consultant-call-history/consultant-call-history.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {NgxPaginationModule} from 'ngx-pagination';
-import { Ng2OrderModule } from 'ng2-order-pipe';
 
 import { ConsultantCallHistoryRoutingModule } from './consultant-call-history-routing.module';
 import { PageHeaderModule, SharedPipesModule, ActionListModule } from '../../shared';
@@ -10,7 +9,7 @@ import { ConsultantCallHistoryComponent } from './consultant-call-history.compon
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-    imports: [NgbModule,CommonModule,NgxPaginationModule,Ng2OrderModule, ConsultantCallHistoryRoutingModule, FormsModule, PageHeaderModule, SharedPipesModule, ActionListModule,NgbModule],
+    imports: [NgbModule,CommonModule,NgxPaginationModule, ConsultantCallHistoryRoutingModule, FormsModule, PageHeaderModule, SharedPipesModule, ActionListModule,NgbModule],
     declarations: [ConsultantCallHistoryComponent]
 })
 export class ConsultantCallHistoryModule {}
