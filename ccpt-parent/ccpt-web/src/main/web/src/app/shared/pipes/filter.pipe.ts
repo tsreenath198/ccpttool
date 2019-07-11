b9212c9b4199c74dd096021e2d59fc8fa18cf5fa
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2019-07-11 16:41:37 +0530

    Search an every field

--- ccpt-parent/ccpt-web/src/main/web/src/app/shared/pipes/filter.pipe.ts
+++ ccpt-parent/ccpt-web/src/main/web/src/app/shared/pipes/filter.pipe.ts
@@ -5,13 +5,13 @@ import { Pipe, PipeTransform, Injectable } from '@angular/core';
 })
 @Injectable()
 export class FilterPipe implements PipeTransform {
-    transform(items: any[], field: string, value: string): any[] {
+    transform(items: any[], value: string): any[] {
         if (!items) {
             return [];
         }
-        if (!field || !value) {
-            return items;
-        }
+        // if (!field || !value) {
+        //     return items;
+        // }
         //item[field].toLowerCase().indexOf(value.toLowerCase()) !== -1
         return items.filter(item => JSON.stringify(item).toLowerCase().indexOf(value.toLowerCase()) !== -1);
     }
