f1e96fdf80fae7d33aaae9cc24eb84b46da135fe
pavankumar2107 <36192034+pavankumar2107@users.noreply.github.com>
2019-07-18 17:12:21 +0530

    Fix Update of AdditionalProperty

--- ccpt-parent/ccpt-server/src/main/java/com/ccpt/model/AdditionalProperty.java
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/model/AdditionalProperty.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,6 +15,7 @@ import lombok.ToString;
 @ToString
 @Entity
 @Table
+@JsonIgnoreProperties({ "id", "key" })
 public class AdditionalProperty extends IDEntity {
 
 	/**
