668563de12cc6dbdedd8da4e263725d831129208
pavankumar2107 <36192034+pavankumar2107@users.noreply.github.com>
2019-07-30 10:51:29 +0530

    backend changes

--- ccpt-parent/ccpt-server/src/main/java/com/ccpt/controller/LoginController.java
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/controller/LoginController.java
@@ -1,9 +1,6 @@
 package com.ccpt.controller;
 
 import javax.security.sasl.AuthenticationException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.ValidationException;
 
@@ -13,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,8 +30,7 @@ public class LoginController extends BaseController<LoginDTO, Login, Integer> {
 	private LoginService loginService;
 
 	@PostMapping(CCPTConstants.LOGIN)
-	public ResponseEntity<Login> login(@RequestBody Login login, HttpSession session,
-			HttpServletResponse httpServletResponse) throws AuthenticationException {
+	public ResponseEntity<Login> login(@RequestBody Login login, HttpSession session) throws AuthenticationException {
 		String username = login.getUsername();
 		String password = login.getPassword();
 		session.setAttribute("username", username);
@@ -44,64 +39,13 @@ public class LoginController extends BaseController<LoginDTO, Login, Integer> {
 		if (count == 1) {
 			Login log = loginService.login(username, password);
 			if (log != null) {
-				if (login.isCheck()) {
-					Cookie user = new Cookie("ccpt_key", log.getUsername());
-					Cookie pwd = new Cookie("ccpt_pwd", log.getPassword());
-					httpServletResponse.addCookie(user);
-					httpServletResponse.addCookie(pwd);
-					log.setCheck(true);
-					Cookie check = new Cookie("check", "true");
-					httpServletResponse.addCookie(check);
-				} else {
-					disableCookies(httpServletResponse);
-				}
 				return new ResponseEntity<Login>(log, HttpStatus.OK);
 			}
-			disableCookies(httpServletResponse);
 			throw new AuthenticationException("Invalid  password");
 
 		} else {
-			disableCookies(httpServletResponse);
 			throw new AuthenticationException("Invalid  username");
 		}
-	}
-
-	private void disableCookies(HttpServletResponse httpServletResponse) {
-		Cookie ccpt_key = new Cookie("ccpt_key", "");
-		ccpt_key.setMaxAge(0);
-		httpServletResponse.addCookie(ccpt_key);
-		Cookie ccpt_pwd = new Cookie("ccpt_pwd", "");
-		ccpt_pwd.setMaxAge(0);
-		httpServletResponse.addCookie(ccpt_pwd);
-	}
-
-	@GetMapping("/secretLogin")
-	public ResponseEntity<Login> secretLogin(HttpServletRequest httpServletRequest) throws AuthenticationException {
-		String username = null, password = null;
-		boolean check = false;
-		Cookie[] cookies = httpServletRequest.getCookies();
-		if (cookies != null) {
-			for (Cookie cookie : cookies) {
-				if (cookie.getName().equals("ccpt_key")) {
-					username = cookie.getValue();
-				}
-				if (cookie.getName().equals("ccpt_pwd")) {
-					password = cookie.getValue();
-				}
-				if (cookie.getName().equals("check")) {
-					check = true;
-				}
-			}
-			Login log = loginService.login(username, password);
-			if (log != null && check == true) {
-				log.setCheck(check);
-				return new ResponseEntity<Login>(log, HttpStatus.OK);
-			} else {
-				throw new AuthenticationException("No Cookies Found");
-			}
-		}
-		return null;
-
 	}
 
 	@Override
