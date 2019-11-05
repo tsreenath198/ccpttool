082eb35ff6126b1a5ae1bedec9d76cd07f298abe
anuraguskcorp <anurag.uskcorp@gmail.com>
2019-11-05 13:56:50 +0530

    get empty data from client position

---
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/model/EmptyUrlStatistics.java
@@ -0,0 +1,15 @@
+package com.ccpt.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class EmptyUrlStatistics {
+	private String generatedCode;
+
+	private String value;
+
+}
