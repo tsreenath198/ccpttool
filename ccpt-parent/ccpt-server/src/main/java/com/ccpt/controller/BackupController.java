65f794716786c25c642c592d7714f24a1dfbba23
pavankumar2107 <36192034+pavankumar2107@users.noreply.github.com>
2019-11-13 12:02:24 +0530

    added updated backupArchives

--- ccpt-parent/ccpt-server/src/main/java/com/ccpt/controller/BackupController.java
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/controller/BackupController.java
@@ -7,7 +7,13 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,6 +33,8 @@ import com.ccpt.constants.CCPTConstants;
 @CrossOrigin
 @RequestMapping(CCPTConstants.BACKUP)
 public class BackupController {
+
+	private static final String INSERT_BACKUP_SQL = "INSERT into backup (backup_date) VALUES (?)";
 
 	@Value("${spring.datasource.driver}")
 	private String driver;
@@ -51,20 +59,62 @@ public class BackupController {
 
 	@GetMapping("/backup")
 	public ResponseEntity<String> backUp() throws SQLException, ValidationException {
-		String[] tables = { "additional_property", "client", "client_application", "client_application_status",
+		String[] tables = { "additional_property", "backup","client", "client_application", "client_application_status",
 				"client_call_history", "client_contact", "client_position", "client_position_status", "consultant",
 				"consultant_call_history", "consultant_status", "email_content", "email_template", "login",
 				"other_contact", "payment", "position_summary", "question", "recruiter", "search", "sms",
 				"sms_template", "upload_file" };
 		Connection from = null, to = null;
+		PreparedStatement statement = null;
 		try {
-			from = getSourceConnection(username, password, url, driver);
-			to = getDestinationConnection(prod_username, prod_password, prod_url, driver);
+			from = getDBConnection(username, password, url, driver);
+			to = getDBConnection(prod_username, prod_password, prod_url, driver);
 			for (int i = 0; i < tables.length; i++) {
 				String table = tables[i];
 				copy(table, from, to);
 				System.out.println(table + " table copied successfully");
 			}
+			statement = from.prepareStatement(INSERT_BACKUP_SQL, Statement.RETURN_GENERATED_KEYS);
+			Calendar cal = Calendar.getInstance();
+			java.sql.Timestamp timestamp = new java.sql.Timestamp(cal.getTimeInMillis());
+			statement.setTimestamp(1, timestamp);
+			statement.executeUpdate();
+		} catch (Exception e) {
+			throw new ValidationException("Exception Occur While BackUp " + e.getMessage());
+		} finally {
+			from.close();
+			to.close();
+		}
+		return new ResponseEntity<String>("Successfully done backup", HttpStatus.OK);
+	}
+
+	@GetMapping("/backUpArchives")
+	public ResponseEntity<String> backUpArchives() throws SQLException, ValidationException {
+		String[] tables = { "additional_property", "client", "client_application", "client_application_status",
+				"client_call_history", "client_contact", "client_position", "client_position_status", "consultant",
+				"consultant_call_history", "consultant_status", "email_content", "email_template", "login",
+				"other_contact", "payment", "position_summary", "question", "recruiter", "search", "sms",
+				"sms_template", "upload_file" };
+
+		Connection from = null, to = null;
+		PreparedStatement statement = null;
+		try {
+			from = getDBConnection(username, password, url, driver);
+			to = getDBConnection(prod_username, prod_password, prod_url, driver);
+			for (int i = 0; i < tables.length; i++) {
+				String table = tables[i];
+				update(table, from, to);
+				System.out.println(table + " table updated successfully");
+			}
+			statement = to.prepareStatement(INSERT_BACKUP_SQL, Statement.RETURN_GENERATED_KEYS);
+			Calendar cal = Calendar.getInstance();
+			java.sql.Timestamp timestamp = new java.sql.Timestamp(cal.getTimeInMillis());
+			statement.setTimestamp(1, timestamp);
+			statement.executeUpdate();
+
+			statement = from.prepareStatement(INSERT_BACKUP_SQL, Statement.RETURN_GENERATED_KEYS);
+			statement.setTimestamp(1, timestamp);
+			statement.executeUpdate();
 
 		} catch (Exception e) {
 			throw new ValidationException("Exception Occur While BackUp " + e.getMessage());
@@ -75,14 +125,43 @@ public class BackupController {
 		return new ResponseEntity<String>("Successfully done backup", HttpStatus.OK);
 	}
 
-	private static Connection getDestinationConnection(String username, String password, String url, String driver)
-			throws Exception {
-		Class.forName(driver);
-		Connection conn = DriverManager.getConnection(url, username, password);
-		return conn;
+	private static void update(String table, Connection from, Connection to) throws SQLException, ParseException {
+
+		Date lastUpdatedDate = getBackupDateFromSourceDB(from);
+		if (lastUpdatedDate != null) {
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(lastUpdatedDate);
+			cal.add(Calendar.DATE, 0);
+			lastUpdatedDate = cal.getTime();
+		} else {
+			String strDate = "2019-01-01 00:00:00";
+			lastUpdatedDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(strDate);
+		}
+
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+		String selectSQL = "select * from ".concat(table).concat(" where created_date >= ' ")
+				.concat(dateFormat.format(lastUpdatedDate)).concat("'");
+		System.out.println("sql:" + selectSQL);
+		try (PreparedStatement s1 = from.prepareStatement(selectSQL); ResultSet rs = s1.executeQuery()) {
+			ResultSetMetaData meta = rs.getMetaData();
+			List<String> columns = new ArrayList<>();
+			for (int i = 1; i <= meta.getColumnCount(); i++)
+				columns.add(meta.getColumnName(i));
+			try (PreparedStatement s2 = to
+					.prepareStatement("INSERT INTO " + table + " (" + columns.stream().collect(Collectors.joining(", "))
+							+ ") VALUES (" + columns.stream().map(c -> "?").collect(Collectors.joining(", ")) + ")")) {
+				while (rs.next()) {
+					for (int i = 1; i <= meta.getColumnCount(); i++)
+						s2.setObject(i, rs.getObject(i));
+					s2.addBatch();
+				}
+				s2.executeBatch();
+			}
+		}
 	}
 
-	private Connection getSourceConnection(String username, String password, String url, String driver)
+	private static Connection getDBConnection(String username, String password, String url, String driver)
 			throws Exception {
 		Class.forName(driver);
 		Connection conn = DriverManager.getConnection(url, username, password);
@@ -111,6 +190,20 @@ public class BackupController {
 				s2.executeBatch();
 			}
 		}
+	}
+
+	private static Date getBackupDateFromSourceDB(Connection conn) throws SQLException {
+		Statement stmt = null;
+		stmt = conn.createStatement();
+		Date date = null;
+		String sql = "SELECT * FROM backup order by id DESC limit 1";
+		ResultSet rs = stmt.executeQuery(sql);
+		while (rs.next()) {
+			Timestamp timestamp = rs.getTimestamp("backup_date");
+			if (timestamp != null)
+				date = new java.util.Date(timestamp.getTime());
+		}
+		return date;
 	}
 
 }
---
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/model/Backup.java
@@ -0,0 +1,32 @@
+package com.ccpt.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table
+@Entity
+@Getter
+@Setter
+@ToString
+public class Backup {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreationTimestamp
+	protected Date backupDate;
+}
