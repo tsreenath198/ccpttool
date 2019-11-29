c1d8ed95fd03a65d50218558f3cc7a5a6ce827cf
pavankumar2107 <36192034+pavankumar2107@users.noreply.github.com>
2019-11-29 12:06:00 +0530

    removed caByStatusList,consultantCallHistoryList,clientCallHistoryList from DashboardModel.java

--- ccpt-parent/ccpt-server/src/main/java/com/ccpt/model/DashboardModel.java
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/model/DashboardModel.java
@@ -14,9 +14,6 @@ public class DashboardModel {
 	private List<InterviewSummaryStatistics> interviewSummaryStatistics;
 	private List<OpenClientPosition> openClientPositions;
 	private List<OpenClientPosition> dyingClientPositions;
-	private List<CAByStatusHelper> caByStatusList;
-	private List<CallHistorySummaryStatistics> consultantCallHistoryList;
-	private List<CallHistorySummaryStatistics> clientCallHistoryList;
 	private List<DashboardCAStatistics> dashboardCAStatistics;
 	private List<PaymentStatistics> paymentStatistics;
 }
