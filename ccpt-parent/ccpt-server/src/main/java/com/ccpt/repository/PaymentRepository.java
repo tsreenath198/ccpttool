8b5208bf543b856ebf5d83dd822e530e8b7c7236
pavankumar2107 <36192034+pavankumar2107@users.noreply.github.com>
2020-01-17 11:06:24 +0530

    changed query

--- ccpt-parent/ccpt-server/src/main/java/com/ccpt/repository/PaymentRepository.java
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/repository/PaymentRepository.java
@@ -9,7 +9,9 @@ import com.ccpt.model.PaymentStatistics;
 
 public interface PaymentRepository extends BaseRepository<Payment, Integer> {
 
-	@Query(value = "SELECT id as id ,generatedCode as companyName,candidateName as candidateName,joiningDate as joiningDate, amountReceivable as amountReceivable ,DATEDIFF(curdate(), joiningDate) as pendingSince FROM Payment where paidStatus='Unpaid'")
+	@Query(value = "SELECT id as id ,generatedCode as companyName,candidateName as candidateName,joiningDate as joiningDate, "
+			+ "amountReceivable as amountReceivable ,DATEDIFF(curdate(), joiningDate) as pendingSince FROM Payment"
+			+ " where paidStatus='Unpaid' and activeFlag=0")
 	List<PaymentStatistics> get();
 
 }
