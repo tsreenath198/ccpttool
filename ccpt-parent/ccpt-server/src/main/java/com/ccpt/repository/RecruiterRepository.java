9bc9426bd5162972f7c711cb7ffff76923d0a2d1
pavan.uskcorp <pavan.uskcorp@gmail.com>
2021-10-19 13:17:11 +0530

    added left join on getAllCreators query

--- ccpt-parent/ccpt-server/src/main/java/com/ccpt/repository/RecruiterRepository.java
+++ ccpt-parent/ccpt-server/src/main/java/com/ccpt/repository/RecruiterRepository.java
@@ -12,7 +12,7 @@ public interface RecruiterRepository extends BaseRepository<Recruiter, Integer> 
 	@Query("SELECT r FROM Recruiter r WHERE fullname=:name AND active_flag=1")
 	Recruiter getRecruiterFromName(@Param(value = "name") String name);
 
-	@Query("SELECT r.id as id,r.aliasName as name, r.email as email,count(cp.assignedTo) as count FROM Recruiter r,ClientPosition cp WHERE cp.assignedTo=r.id AND r.activeFlag=1 GROUP BY cp.assignedTo ORDER BY r.createdDate DESC")
+	@Query(value="SELECT r.id as id,r.alias_name as name, r.email as email,count(cp.assigned_to) as count FROM Recruiter r LEFT JOIN Client_Position cp ON r.id =cp.assigned_to WHERE  r.active_flag=1 GROUP BY cp.assigned_to ORDER BY r.created_date",nativeQuery = true)
 	List<RecruiterDropdownStatistics> getAllCreators();
 
 	@Query(value = "SELECT email FROM Recruiter r WHERE role IN('Admin','Senior Recruiter') AND active_flag=1", nativeQuery = true)
